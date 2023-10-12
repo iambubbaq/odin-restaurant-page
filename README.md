f0c3cda9ef232ff3a78b7253001c99a3c47282ba
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-10-12 11:24:51 -0600

    add file structure and functional webpack with bones of main page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-restaurant-page
 Restaurant page to delve deeper into intermediate javascript with a focus on webpack and modular javascript
+
+Photo by Jer Chung: https://www.pexels.com/photo/bowl-of-fried-food-and-fried-egg-2059151/
---
+++ src/homePage.js
@@ -0,0 +1,29 @@
+function homePage() {
+    //setup page content div
+    let content = document.getElementById("content");
+    let pageContent = document.createElement('div');
+    pageContent.classList.add('pageContent');
+
+    //create and append header
+    let header = document.createElement('h1');
+    header.textContent = "Shǐshī Bufffet Home";
+    pageContent.appendChild(header);
+
+    //create and append image
+    let img = document.createElement('img');
+    img.src = "../img/BeefandEgg.jpg";
+    img.classList.add('restaurantIMG');
+    img.height = "200";
+    img.width = "300";
+    pageContent.appendChild(img);
+
+    //create and append slogan
+    let slogan = document.createElement('p');
+    slogan.textContent = "This Chinese Buffet will have coming back each week!";
+    pageContent.appendChild(slogan);
+
+    //append page content to main div
+    content.appendChild(pageContent);
+}
+
+export default homePage;
